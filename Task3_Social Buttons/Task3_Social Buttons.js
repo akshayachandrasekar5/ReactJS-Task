621a34f848ccac8fb8e9168c886df518d1b794cf
akshayachandrasekar5 <154856750+akshayachandrasekar5@users.noreply.github.com>
2024-08-13 22:55:46 +0530

    Social Button

---
+++ Task3_Social Buttons/Task3_Social Buttons.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import './Task3.css';
+function Task3_Social Buttons(){
+    let button=()=>{
+        let like=document.getElementById('like').value;
+         
+        alert("You have responded");
+    }    
+    return(
+        <>
+        <div className="Bg">
+            <center>
+                <h1>
+                    Social Buttons
+                </h1>
+                <input type="button" id="like" value="Like" onClick={button}/>&nbsp;&nbsp;&nbsp;
+                <input type="button" id="comment" value="Comment" onClick={button}/>&nbsp;&nbsp;&nbsp;
+                <input type="button" id="share" value="Share" onClick={button}/>
+            </center>
+        </div>
+        </>
+    )
+}
+export default Task3_Social Buttons;
